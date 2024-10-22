f6f044b632194d33f59fbd4d7ec82bd4e6cde5f4
reborn_star1 <rebornstar677@gmail.com>
2024-10-22 22:48:46 +0530

    First Program in Spring is Runned

---
+++ runnerz/src/main/java/dev/imsanjaypaul/runnerz/FirstEntry.java
@@ -0,0 +1,10 @@
+package dev.imsanjaypaul.runnerz;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstEntry {
+    public String getWelcomeMessage() {
+        return "This is my first code in the spring and I'm very proud of this. i will look forward to grow in this stack and contribute to the community";
+    }
+}
--- runnerz/src/main/java/dev/imsanjaypaul/runnerz/RunnerzApplication.java
+++ runnerz/src/main/java/dev/imsanjaypaul/runnerz/RunnerzApplication.java
@@ -2,12 +2,14 @@ package dev.imsanjaypaul.runnerz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RunnerzApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunnerzApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(RunnerzApplication.class, args);
+		FirstEntry firstEntry = (FirstEntry) context.getBean("firstEntry");
+		System.out.println(firstEntry.getWelcomeMessage());
 	}
-
 }
